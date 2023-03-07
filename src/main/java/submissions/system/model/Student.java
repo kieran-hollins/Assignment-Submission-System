5fa84050abfd3e49626436922235a3cd90b5e18a
Kieran Hollins <khollins123@gmail.com>
2023-03-07 23:57:02 +0000

    fixed name

--- src/main/java/submissions/system/model/Student.java
+++ src/main/java/submissions/system/model/Student.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "assignments")
+@Table(name = "students")
 public class Student implements Serializable {
 
     @Id

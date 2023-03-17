43787815e6f40fc04cefbbcc9b68e52b4287e1d7
Kieran Hollins <khollins123@gmail.com>
2023-03-17 17:25:28 +0000

    added module class

--- src/main/java/submissions/system/model/Module.java
+++ src/main/java/submissions/system/model/Module.java
@@ -1,5 +1,41 @@
 package submissions.system.model;
 
-public class Module {
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "modules")
+public class Module implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "module_id")
+    private long id;
+
+    @Column(name = "module_name")
+    private String name;
+
+    @Column(name = "module_Course_id")
+    private long courseId;
+
+    @Column(name = "credits")
+    private int credits;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return name;
+    }
+
+    public void setCourseName(String name) {
+        this.name = name;
+    }
 
 }

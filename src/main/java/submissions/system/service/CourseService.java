9bfd00aaf2a1c26c7bece05bf04ef7783c23f964
Kieran Hollins <khollins123@gmail.com>
2023-03-23 17:10:08 +0000

    Added course service

---
+++ src/main/java/submissions/system/service/CourseService.java
@@ -0,0 +1,17 @@
+package submissions.system.service;
+
+import java.util.List;
+import submissions.system.model.*;
+
+public interface CourseService {
+    List<Course> getAllCourses();
+
+    Course createCourse(Course course);
+
+    Course getCourseById(long id);
+
+    Course updateCourseById(long id, Course course);
+
+    String deleteCourseById(long id);
+
+}
